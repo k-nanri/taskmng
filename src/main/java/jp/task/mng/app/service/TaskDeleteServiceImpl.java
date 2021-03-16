8fa9318e6676828a5112bd29259ec2a141a980d7
k-nanri <kotaro.nanri15@gmail.com>
2021-03-16 20:29:55 +0900

    タスク削除のサービスクラスに実装メソッドを追加

--- src/main/java/jp/task/mng/app/service/TaskDeleteServiceImpl.java
+++ src/main/java/jp/task/mng/app/service/TaskDeleteServiceImpl.java
@@ -1,5 +1,14 @@
 package jp.task.mng.app.service;
 
+import org.springframework.http.ResponseEntity;
+
+import jp.task.mng.app.model.TodoId;
+
 public class TaskDeleteServiceImpl implements TaskDeleteService {
+
+    @Override
+    public ResponseEntity<Void> removeTask(TodoId tagetId) {
+        return null;
+    }
 
 }
